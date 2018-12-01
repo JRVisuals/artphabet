ef321ee3b79ff3e9b1b9fd8e3a2ea78d62dc21d3
Jose R. Rodriguez <me@JRVisuals.com>
2018-12-01 15:37:40 -0500

    first pass at wiring in typescript - new files

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import registerServiceWorker from './registerServiceWorker';
+
+import App from './App';
+
+import './index.css';
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
