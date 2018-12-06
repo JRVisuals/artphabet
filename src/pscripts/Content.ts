5ef68f14bfbc99662d4ffb5e2148fbfc292f2c37
Jose R. Rodriguez <me@JRVisuals.com>
2018-12-05 20:25:50 -0500

    add typescript to Content file, update content file to include unique motif size

---
+++ src/pscripts/Content.ts
@@ -0,0 +1,223 @@
+// Type interface for content blocks
+interface IContentBlock {
+    meta: {
+        author: string,
+        title: string,
+        type: string,
+        pallette: string,
+        artist: string,
+    };
+
+    rendering: {
+        canvasPixelDimension: { width: number, height: number},
+        smallImg: number,
+        largeImg: number,
+        jitter: number,
+        pallette: string,
+        motifPrefix: string,
+        motifPattern: number[],
+        motifRotation: number,
+        motifSize: number,
+    };
+
+    timing: {
+        shortPause: number,
+        longPause: number,
+        transitionTime: number,
+    };
+
+    text: string;
+}
+
+// Define individual content blocks
+
+const journalHillsContent: IContentBlock = {
+        meta: {
+            author: "Charles E. Burchfield",
+            title: "Journals, Volume 31",
+            type: "Journal",
+            pallette: "Late Afternoon in the Hills",
+            artist: "Charles E. Burchfield",
+        },
+
+        rendering: {
+            canvasPixelDimension: {width: 1300, height: 800},
+            smallImg: 125,
+            largeImg: 200,
+            jitter: 5,
+            pallette: "burch_hills_blur",
+            motifPrefix: "hills_",
+            motifPattern: [0, 0, 0, 0, 1],
+            motifRotation: 0,
+            motifSize: 200,
+        },
+
+        timing: {
+            shortPause: 10,
+            longPause: 50,
+            transitionTime: 250,
+        },
+
+        text:
+            "A dream – Working someplace to the north of here; \
+            a channel like depression in the earth with a few woods; \
+            a trolley line took us home at night, in my spare time I painted having brought my materials along; \
+            one I sketched was the weird setting sun thru the doorway of a deserted house. \
+            Towards evening when everyone seemed getting ready to go home, \
+            I noticed in the hot yellow afterglow; bluebirds clinging to an old board fence; \
+            they seemed asleep and I was able to pick them off and put them in my pocket. \
+            Thunder was heard rumbling; in the gathering gloom I noticed a kite in the sky, \
+            then more & more appeared. It seemed as if the boys flying them had lost control \
+            of them [as] the strings sagged low over the earth. \
+            I was able to gather them all together in my hand. \
+            The kites were falling, I gave the string a jerk and stepped back a few paces; \
+            they spring up in the air again radiating from my hand; \
+            I was considering whether to tie all the strings together for a joke when the six o’clock car whistled.  \
+            It seemed as if my duty was to see that all left the place on time, and that I myself, \
+            the last to leave had to catch the car in the rain.  \
+            Then I remembered that I had left my sketching material on the ground somewhere.  \
+            The sketches I had removed from the portfolio and laid in some swamp grass, \
+            the portfolio in some bushes.  \
+            On finding the latter it was submerged in water a few inches; \
+            anxious, I sought the sketches only to find them entirely covered with water.  \
+            I lifted them carefully out; \
+            the one of the sunset had been soaked and the paint partly transfigured & the back of another.  \
+            By this time of course the car was gone & I had the prospects of waiting an hour; \
+            but now I saw that a young man from town had his car with him. \
+            [Though] I had never asked him, still I asked him to take me home with him. \
+            He consented but was waiting for his mother-in-law to come.  \
+            The dream broke off here –",
+};
+
+const poemStayGold: IContentBlock = {
+    meta: {
+        author: "Robert Frost",
+        title: "Nothing Gold Can Stay",
+        type: "Poem",
+        pallette: "The Insect Chorus",
+        artist: "Charles E. Burchfield",
+    },
+
+    rendering: {
+        canvasPixelDimension: {width: 500, height: 500},
+        smallImg: 100,
+        largeImg: 150,
+        jitter: 0,
+        pallette: "burch_insect_blur",
+        motifPrefix: "burch_",
+        motifPattern: [4, 5, 6, 6, 6],
+        motifRotation: .25,
+        motifSize: 200,
+    },
+
+    timing: {
+        shortPause: 100,
+        longPause: 300,
+        transitionTime: 1000,
+    },
+
+    text:
+        "Nature's first green is gold, \
+        Her hardest hue to hold. \
+        Her early leaf's a flower; \
+        But only so an hour. \
+        Then leaf subsides to leaf. \
+        So Eden sank to grief, \
+        So dawn goes down to day. \
+        Nothing gold can stay.",
+};
+
+const poemColemansBed: IContentBlock = {
+    meta: {
+        author: "David Whyte",
+        title: "Coleman's Bed",
+        type: "Poem",
+        pallette: "The Insect Chorus",
+        artist: "Charles E. Burchfield",
+    },
+
+    rendering: {
+        canvasPixelDimension: { width: 1400, height: 750},
+        smallImg: 150,
+        largeImg: 300,
+        jitter: 2,
+        pallette: "burch_insect_blur",
+        motifPrefix: "burch_",
+        motifPattern: [0, 4, 1, 5, 3, 4, 5 ],
+        motifRotation: .25,
+        motifSize: 200,
+    },
+
+    timing: {
+        shortPause: 10,
+        longPause: 50,
+        transitionTime: 200,
+    },
+
+    text:
+        "Make a nesting now, a place to which \
+        the birds can come, think of Kevin's \
+        prayerful palm holding the blackbird's egg \
+        and be the one, looking out from this place \
+        who warms interior forms into light. \
+        Feel the way the cliff at your back \
+        gives shelter to your outward view \
+        and then bring in from those horizons \
+        all discordant elements that seek a home. \
+        \
+        Be taught now, among the trees and rocks, \
+        how the discarded is woven into shelter, \
+        learn the way things hidden and unspoken \
+        slowly proclaim their voice in the world. \
+        Find that far inward symmetry \
+        to all outward appearances, apprentice \
+        yourself to yourself, begin to welcome back \
+        all you sent away, be a new annunciation, \
+        make yourself a door through which \
+        to be hospitable, even to the stranger in you. \
+        \
+        See with every turning day, \
+        how each season makes a child \
+        of you again, wants you to become \
+        a seeker after rainfall and birdsong, \
+        watch now, how it weathers you \
+        to a testing in the tried and true, \
+        admonishes you with each falling leaf, \
+        to be courageous, to be something \
+        that has come through, to be the last thing \
+        you want to see before you leave the world. \
+        \
+        Above all, be alone with it all, \
+        a hiving off, a corner of silence \
+        amidst the noise, refuse to talk, \
+        even to yourself, and stay in this place \
+        until the current of the story \
+        is strong enough to float you out. \
+        \
+        Ghost then, to where others \
+        in this place have come before, \
+        under the hazel, by the ruined chapel, \
+        below the cave where Coleman slept, \
+        become the source that makes \
+        the river flow, and then the sea \
+        beyond. Live in this place \
+        as you were meant to and then, \
+        surprised by your abilities, \
+        become the ancestor of it all, \
+        the quiet, robust and blessed Saint \
+        that your future happiness \
+        will always remember.",
+};
+
+// Drop content blocks into a content object for export
+const  Content = {
+
+    journal_hills: journalHillsContent,
+
+    poem_staygold: poemStayGold,
+
+    poem_colemansbed: poemColemansBed,
+
+};
+
+export default Content;
